e019f390c5779efe55c1f2632255d8fd21e32dd3
Henna Vahatiitto <hnnamv@gmail.com>
2019-12-19 19:40:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,11 +11,23 @@ def summon_captain_planet(veggies)# code an argument here
     call.capitalize + "!"
   end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
 end
 
-def find_the_cheese# code an argument here
+def long_planteer_calls(long_planteer_calls)# code an argument here
+  # Your code here
+  answer = false
+  long_planteer_calls.each do |call|
+    if call.length > 4
+      answer = true
+    end
+  end
+    answer
+end
+
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
